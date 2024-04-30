535319de191d9e0dfdff9ae90b9ae261b89776c5
Ly-TE <528179877@qq.com>
2024-04-30 19:59:08 +0800

    框架搭建

--- test_cases/test_api.py
+++ test_cases/test_api.py
@@ -2,24 +2,33 @@ import unittest
 import json
 from api_keys.keys import Apikeys
 from ddt import ddt, file_data
-from urllib.parse import unquote  # 添加导入语句
 
 
 @ddt
 class TestApi(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.api = Apikeys()
+        cls.account_token = None
+
     @file_data('../test_data/login.yaml')
-    def test_login(self, **kwargs):
-        path = kwargs.get('path')
-        data = kwargs.get('data')
-
-        api = Apikeys()
-
-        rep = api.do_post(path=path, data=data)
+    def test_01_login(self, **kwargs):
+        rep = self.api.do_post(**kwargs)
 
         # 将返回结果解析为JSON对象
         response_json = json.loads(rep.text)
 
         # 解码其中的文本部分
+        decoded_text = json.dumps(response_json, ensure_ascii=False)
+        print(decoded_text)
+        self.account_token = response_json['data']['login_info']['account_token']
+
+        print(self.account_token)
+
+    @file_data('../test_data/bannerlist.yaml')
+    def test_02_bannerlist(self, **kwargs):
+        rep = self.api.do_get(**kwargs)
+        response_json = json.loads(rep.text)
         decoded_text = json.dumps(response_json, ensure_ascii=False)
         print(decoded_text)
 
